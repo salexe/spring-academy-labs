c7d51aa228197fad3d675aa22fab3b0c3151c4ca
salexe <serban.alexe@gmail.com>
2025-08-13 18:23:04 +0200

    done: MODULE 2 (Java Configuration) / Spring Framework Essentials

--- lab/12-javaconfig-dependency-injection/src/main/java/config/RewardsConfig.java
+++ lab/12-javaconfig-dependency-injection/src/main/java/config/RewardsConfig.java
@@ -2,8 +2,21 @@ package config;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import rewards.RewardNetwork;
+import rewards.internal.RewardNetworkImpl;
+import rewards.internal.account.AccountRepository;
+import rewards.internal.account.JdbcAccountRepository;
+import rewards.internal.restaurant.JdbcRestaurantRepository;
+import rewards.internal.restaurant.RestaurantRepository;
+import rewards.internal.reward.JdbcRewardRepository;
+import rewards.internal.reward.RewardRepository;
+
 /**
- * TODO-00: In this lab, you are going to exercise the following:
+ * dTODO-00: In this lab, you are going to exercise the following:
  * - Creating Spring configuration class
  * - Defining bean definitions within the configuration class
  * - Specifying the dependency relationships among beans
@@ -11,10 +24,10 @@ import javax.sql.DataSource;
  * - Creating Spring application context in the test code
  *   (WITHOUT using Spring testContext framework)
  *
- * TODO-01: Make this class a Spring configuration class
+ * dTODO-01: Make this class a Spring configuration class
  * - Use an appropriate annotation.
  *
- * TODO-02: Define four empty @Bean methods, one for the
+ * dTODO-02: Define four empty @Bean methods, one for the
  *          reward-network and three for the repositories.
  * - The names of the beans should be:
  *   - rewardNetwork
@@ -22,7 +35,7 @@ import javax.sql.DataSource;
  *   - restaurantRepository
  *   - rewardRepository
  *
- * TODO-03: Inject DataSource through constructor injection
+ * dTODO-03: Inject DataSource through constructor injection
  * - Each repository implementation has a DataSource
  *   property to be set, but the DataSource is defined
  *   elsewhere (TestInfrastructureConfig.java), so you
@@ -30,7 +43,7 @@ import javax.sql.DataSource;
  *   that accepts a DataSource parameter.
  * - As it is the only constructor, @Autowired is optional.
  *
- * TODO-04: Implement each @Bean method to contain the code
+ * dTODO-04: Implement each @Bean method to contain the code
  *          needed to instantiate its object and set its
  *          dependencies
  * - You can create beans from the following implementation classes
@@ -42,9 +55,41 @@ import javax.sql.DataSource;
  *   not an implementation.
  */
 
+@Configuration
 public class RewardsConfig {
 
 	// Set this by adding a constructor.
 	private DataSource dataSource;
 
+	@Autowired
+	public RewardsConfig(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
+
+	@Bean
+	public RewardNetwork rewardNetwork() {
+		return new RewardNetworkImpl(accountRepository(), restaurantRepository(), rewardRepository());
+	}
+
+	@Bean
+	public AccountRepository accountRepository() {
+		final JdbcAccountRepository jdbcAccountRepository = new JdbcAccountRepository();
+		jdbcAccountRepository.setDataSource(this.dataSource);
+		return jdbcAccountRepository;
+	}
+	
+	@Bean
+	public RestaurantRepository restaurantRepository() {
+		final JdbcRestaurantRepository jdbcRestaurantRepository = new JdbcRestaurantRepository();
+		jdbcRestaurantRepository.setDataSource(this.dataSource);
+		return jdbcRestaurantRepository;
+	}
+	
+	@Bean
+	public RewardRepository rewardRepository() {
+		final JdbcRewardRepository jdbcRewardRepository = new JdbcRewardRepository();
+		jdbcRewardRepository.setDataSource(dataSource);
+		return jdbcRewardRepository; 
+	}
+	
 }
--- lab/12-javaconfig-dependency-injection/src/test/java/config/RewardsConfigTests.java
+++ lab/12-javaconfig-dependency-injection/src/test/java/config/RewardsConfigTests.java
@@ -1,12 +1,24 @@
 package config;
 
-import org.assertj.core.api.Fail;
-import org.mockito.Mockito;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import javax.sql.DataSource;
 import java.lang.reflect.Field;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import javax.sql.DataSource;
+
+import org.assertj.core.api.Fail;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import rewards.RewardNetwork;
+import rewards.internal.RewardNetworkImpl;
+import rewards.internal.account.AccountRepository;
+import rewards.internal.account.JdbcAccountRepository;
+import rewards.internal.restaurant.JdbcRestaurantRepository;
+import rewards.internal.restaurant.RestaurantRepository;
+import rewards.internal.reward.JdbcRewardRepository;
+import rewards.internal.reward.RewardRepository;
 
 /**
  * Unit test the Spring configuration class to ensure it is creating the right
@@ -17,13 +29,13 @@ public class RewardsConfigTests {
 	// Provide a mock object for testing
 	private DataSource dataSource = Mockito.mock(DataSource.class);
 
-	// TODO-05: Run the test
+	// dTODO-05: Run the test
 	// - Uncomment the code below between /* and */
 	// - If you have implemented RewardsConfig as requested it should compile.
 	// - Fix RewardsConfig if necessary.
 	// - Now run the test, it should pass.
 
-	/*
+	
 	private RewardsConfig rewardsConfig = new RewardsConfig(dataSource);
 
 	@Test
@@ -43,7 +55,7 @@ public class RewardsConfigTests {
 		assertTrue(rewardsRepository instanceof JdbcRewardRepository);
 		checkDataSource(rewardsRepository);
 	}
-	*/
+	
 
 	/**
 	 * Ensure the data-source is set for the repository. Uses reflection as we do
